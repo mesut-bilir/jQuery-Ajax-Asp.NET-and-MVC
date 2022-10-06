9b76339befb17efb24f47de2a999c4d844c8cc9d
mesudbilir <mesudbilir@gmail.com>
2022-10-06 12:24:18 +0300

    ss

--- test.js
+++ test.js
@@ -1,2 +1,5 @@
-test 
-Comment 
+Yaptığımız değişiklikleri karşı tarafa yani github’a göndermeden önce kendi bilgisayarımızda staged durumuna getirmeliyiz. Bu işlemi hem komut satırında hem de VSCode üzerinde yapabiliriz.
+
+VSCode üzerinde yapabilmek için aşağıdaki komutu çalıştırmalıyız. Ama dikkat edin staged durumuna getirmek istediğimiz dosya ile aynı dizinde olmalısınız.
+
+xxx
